41fca9d5fb2f78c91f21953969df0c28257d3161
devashish bakare <devashishbakare@gmail.com>
2022-07-28 11:40:23 +0530

    Added Server

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const port = 8000;
+const app = express();
+
+
+
+
+
+
+
+
+app.listen(port, function(err){
+    if (err) {
+        console.log("Server Crashed!!");
+        return;
+    }
+    console.log("Server is running successfully on port :", port);
+    return;
+})
